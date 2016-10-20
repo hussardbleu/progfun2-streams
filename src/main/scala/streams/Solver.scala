cb71c971c014051e8526cef669edaa8b77bbf4d2
hussard bleu <bleuhussard@gmail.com>
2016-10-20 17:33:48 +0200

    Solver:  implementing a function from, which, given an initial stream and a set of explored blocks, creates a stream containing the possible paths starting from the head of the initial stream. Then we use from to define a stream of all possible pairs of goal blocks along with their history and pick the shortest one.

--- src/main/scala/streams/Solver.scala
+++ src/main/scala/streams/Solver.scala
@@ -2,6 +2,8 @@ package streams
 
 import common._
 
+import scala.collection.immutable.Stream.cons
+
 /**
  * This component implements the solver for the Bloxorz game
  */
@@ -69,18 +71,26 @@ trait Solver extends GameDef {
    * construct the correctly sorted stream.
    */
   def from(initial: Stream[(Block, List[Move])],
-           explored: Set[Block]): Stream[(Block, List[Move])] = ???
+           explored: Set[Block]): Stream[(Block, List[Move])] =
+    if (initial.isEmpty) Stream.empty
+    else{
+      val more = for {
+        (block, moves) <- initial
+        next <- newNeighborsOnly(neighborsWithHistory(block, moves), explored)
+      } yield next
+      initial #::: from(more, explored ++ (more map{case(block, moves) => block}))
+    }
 
   /**
    * The stream of all paths that begin at the starting block.
    */
-  lazy val pathsFromStart: Stream[(Block, List[Move])] = ???
+  lazy val pathsFromStart: Stream[(Block, List[Move])] = from(List((startBlock, Nil)).toStream, Set())
 
   /**
    * Returns a stream of all possible pairs of the goal block along
    * with the history how it was reached.
    */
-  lazy val pathsToGoal: Stream[(Block, List[Move])] = ???
+  lazy val pathsToGoal: Stream[(Block, List[Move])] = pathsFromStart filter {case(block, moves) => done(block)}
 
   /**
    * The (or one of the) shortest sequence(s) of moves to reach the
@@ -90,5 +100,6 @@ trait Solver extends GameDef {
    * the first move that the player should perform from the starting
    * position.
    */
-  lazy val solution: List[Move] = ???
+  lazy val solution: List[Move] = (pathsToGoal sortWith ((e1, e2) => e1._2.length < e2._2.length)).head._2
+
 }
