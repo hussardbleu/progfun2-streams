a4468ccc87f5712f4af8f3ea9f482978559a8be0
hussard bleu <bleuhussard@gmail.com>
2016-10-20 16:48:14 +0200

    Solver:  implementing a function newNeighborsOnly which, given a stream of Neighbors and Path and already explored Blocks returns a stream consisting only in new Neighbors

--- src/test/scala/streams/BloxorzSuite.scala
+++ src/test/scala/streams/BloxorzSuite.scala
@@ -56,7 +56,14 @@ class BloxorzSuite extends FunSuite {
     }
   }
 
-	test("neighborsWithHistory level 1") {
+
+  test("findChar level 1") {
+    new Level1 {
+      assert(startPos == Pos(1,1))
+    }
+  }
+
+  test("neighborsWithHistory level 1") {
     new Level1 {
       assert(neighborsWithHistory(Block(Pos(1,1),Pos(1,1)), List(Left,Up)).toSet == Set(
         (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
@@ -65,13 +72,20 @@ class BloxorzSuite extends FunSuite {
     }
   }
 
-  test("findChar level 1") {
+  test("newNeighborsOnly level 1") {
     new Level1 {
-      assert(startPos == Pos(1,1))
+      assert(newNeighborsOnly(
+        Set(
+          (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+          (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+        ).toStream,
+
+        Set(Block(Pos(1,2),Pos(1,3)), Block(Pos(1,1),Pos(1,1)))
+      ).toSet == Set(
+        (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+      ))
     }
   }
-
-
 
 	test("optimal solution for level 1") {
     new Level1 {
